8ba964ff167cb713052ea2069a8a4a9ebe5e9311
Naohito Ikeda <ikepo.japon@gmail.com>
2020-04-04 15:56:15 +0900

    ユーザー情報更新機能実装
    # WHAT
    User#updateのアクションを作成、ユーザー情報の更新機能を実装

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -24,7 +25,9 @@
 <body>
     @yield('navbar')
     
-    @yield('content')
+    <div class="container">
+        @yield('content')
+    </div>
 
     @yield('footer')
 
