e3a4ae1a49a9a816ba6e115e36cda0a189e0feda
Naohito Ikeda <ikepo.japon@gmail.com>
2020-04-04 15:04:25 +0900

    ユーザー情報の更新機能実装
    # WHAT
    ・User#Updateのアクションを作成。ユーザー情報の更新機能を実装

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,17 @@ Route::get('/', 'PostsController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+<<<<<<< Updated upstream
+=======
+
+Route::get('users/edit', 'UsersController@edit');
+
+Route::post('users/update', 'UsersController@update');
+
+Route::get('/users/{user_id}', 'UsersController@show');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+>>>>>>> Stashed changes
