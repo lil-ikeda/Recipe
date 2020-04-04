af6a94111286afba67c32740919b5648d1a444ee
Naohito Ikeda <ikepo.japon@gmail.com>
2020-04-04 16:39:11 +0900

    投稿機能の実装
    # WHAT
    ・投稿機能を実装
    ・投稿後一覧ページに遷移はするが、投稿一覧機能は未実装

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,11 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use Auth;
+use Validator;
+
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function index() {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index() 
+    {
         return view('post/index');
+    }
+
+    public function new()
+    {
+        return view('post/new');
+    }
+
+    public function store(Request $request)
+    {
+        //バリデーション（入力値チェック）
+        $validator = Validator::make($request->all() , ['caption' => 'required|max:255', 'photo' => 'required']);
+
+        //バリデーションの結果がエラーの場合
+        if ($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
+
+        // Postモデル作成
+        $post = new Post;
+        $post->caption = $request->caption;
+        $post->user_id = Auth::user()->id;
+
+        $post->save();
+        
+        $request->photo->storeAs('public/post_images', $post->id . '.jpg');
+        
+        // 「/」 ルートにリダイレクト
+        return redirect('/');
     }
 }
